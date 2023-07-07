2e061fe3deea0266fb8e7b4437d308e105cdbc47
joe <joesriprajittichai@gmail.com>
2023-07-07 13:31:16 -0400

    #11 add icons to home edit page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,7 +15,7 @@ const emit = defineEmits([
 const editing = ref(false)
 
 const editBtnMsg = computed(() => {
-  return editing.value ? 'Done' : 'Edit'
+  return editing.value ? 'Done' : 'Edit Workouts'
 })
 
 function addWorkout(e) {
@@ -33,11 +33,17 @@ function addWorkout(e) {
     </header>
     <section class="main" >
       <div v-for="workout in workouts">
-        <div v-if="editing">
-          <input type="text" v-model="workout.name">
-          <button @click="$emit('moveWorkoutUp', workout)">&#8679</button>
-          <button @click="$emit('moveWorkoutDown', workout)">&#8681</button>
-          <button @click="$emit('delWorkout', workout)">X</button>
+        <div v-if="editing" class="input-group">
+          <input v-model="workout.name" type="text" class="form-control">
+          <button type="button" class="btn btn-outline-primary" @click="$emit('moveWorkoutUp', workout)">
+            <img src="/assets/icons/arrow-up-circle-fill.svg" alt="Move Up">
+          </button>
+          <button type="button" class="btn btn-outline-primary" @click="$emit('moveWorkoutDown', workout)">
+            <img src="/assets/icons/arrow-down-circle-fill.svg" alt="Move Down">
+          </button>
+          <button type="button" class="btn btn-outline-primary" @click="$emit('delWorkout', workout)">
+            <img src="/assets/icons/x-circle-fill.svg" alt="Delete">
+          </button>
         </div>
         <div v-else>
           <h2 @click="$emit('clickWorkout', workout)">{{ workout.name }}</h2>
