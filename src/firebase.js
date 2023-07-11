9c5d79113e3140cc66b5147a1aacf0ea1042981c
joe <joesriprajittichai@gmail.com>
2023-07-11 15:53:31 -0400

    signin populates a User, but need signout to test it

--- src/firebase.js
+++ src/firebase.js
@@ -4,8 +4,11 @@ import {
     GoogleAuthProvider,
     getAuth, 
     signInWithRedirect, 
+    getRedirectResult,
     onAuthStateChanged,
 } from "firebase/auth";
+
+import { User } from '/src/types.js'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDCtB516R6TQv2wx-Ikf0-AsMxDlJhJmZw",
@@ -21,21 +24,31 @@ const app = initializeApp(firebaseConfig);
 export async function auth() {
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
+    const user = new User();
 
     onAuthStateChanged(auth, async (user) => {
         if (user) {
             console.log(`user is signed in. User = ${user}`)
         } else {
-            await signInWithRedirect(auth, provider)
-                .then((result) => {
-                    const user = result.user;
-                    console.log(user);
-                }).catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    const email = error.customData.email;
-                    console.error(`signInWithRedirect: code: ${errorCode}, msg: ${errorMessage}, email: ${email}`);
-                });
+            await signInWithRedirect(auth, provider);
+            const result = await getRedirectResult(auth);
+            if (result) {
+                user.email = result.user.providerData.email;
+                console.log(`email: ${user.email}`);
+                user.displayName = result.user.providerData.displayName;
+                console.log(`displayName: ${user.displayName}`);
+            } else {
+                console.error(`get user data from redirect result`)
+            }
+                // .then((result) => {
+                //     const user = result.user;
+                //     console.log(user);
+                // }).catch((error) => {
+                //     const errorCode = error.code;
+                //     const errorMessage = error.message;
+                //     const email = error.customData.email;
+                //     console.error(`signInWithRedirect: code: ${errorCode}, msg: ${errorMessage}, email: ${email}`);
+                // });
         }
     })
 }
