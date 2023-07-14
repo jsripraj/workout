4ab777b6940c9fce5ddf14a7924e661d0af28b81
joe <joesriprajittichai@gmail.com>
2023-07-14 11:14:11 -0400

    finished cleanup

--- src/types.js
+++ src/types.js
@@ -4,7 +4,25 @@ export const pageTypes = {
 }
 
 export const _docNameHistory = "history";
+
 export const _docNameCurrent = "current";
+
+export const _startYear = 2000;
+
+export const _months = {
+    January: 0,
+    February: 1,
+    March: 2, 
+    April: 3, 
+    May: 4, 
+    June: 5, 
+    July: 6, 
+    August: 7, 
+    September: 8, 
+    October: 9, 
+    November: 10, 
+    December: 11
+  };
 
 export class User {
     constructor(email='', displayName='') {
